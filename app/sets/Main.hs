88d528eca9a3fd0392db6567687aa643888e8ffd
Nikolai Ponomarev <wowasterdev@gmail.com>
2023-09-28 12:30:02 +0300

    Change sets to meet the requirements

--- app/sets/Main.hs
+++ app/sets/Main.hs
@@ -3,23 +3,29 @@ module Main where
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Options.Applicative
+import Text.Pretty.Simple
 
 data Files = Files String String
 
 printUnion :: Set Char -> Set Char -> IO ()
 printUnion set1 set2 = do
     putStr "Union of sets: "
-    print $ Set.toList $ Set.union set1 set2
+    pPrint $ Set.toList $ Set.union set1 set2
 
 printDiff :: Set Char -> Set Char -> IO ()
 printDiff set1 set2 = do
     putStr "Difference of sets: "
-    print $ Set.toList $ Set.difference set1 set2
+    pPrint $ Set.toList $ Set.difference set1 set2
 
 printIntersect :: Set Char -> Set Char -> IO ()
 printIntersect set1 set2 = do
     putStr "Intersection of sets: "
-    print $ Set.toList $ Set.intersection set1 set2
+    pPrint $ Set.toList $ Set.intersection set1 set2
+
+printSize :: Int -> Set Char -> IO ()
+printSize i set = do
+    putStr $ "Size of alphabet " ++ show i ++ " "
+    print $ Set.size set
 
 sets :: Files -> IO ()
 sets (Files file1 file2) = do
@@ -31,6 +37,9 @@ sets (Files file1 file2) = do
             printUnion symbols1 symbols2
             printIntersect symbols1 symbols2
             printDiff symbols1 symbols2
+            printSize 1 symbols1
+            printSize 2 symbols2
+            print $ Set.size $ Set.difference symbols1 symbols2
 
 pFiles :: Parser Files
 pFiles =
